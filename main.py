3c6483f10f0327f78307d49eb851345e026572a7
Alex Laws <tal1286@gmail.com>
2018-10-16 21:13:24 -0700

    mailroom add donations

--- main.py
+++ main.py
@@ -3,21 +3,42 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donor, Donation 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        donor = Donor.select().where(Donor.name == request.form['name']).get()
+
+        if donor:
+            new_donation = Donation(value=request.form['amount'], donor=donor)
+            new_donation.save()
+        else:
+            new_donor = Donor(name=request.form['name'])
+            new_donor.save()
+            new_donation = Donation(value=request.form['amount'],
+                                    donor=new_donor)
+            new_donation.save()
+
+        return render_template('create.jinja2')
+
+    return redirect(url_for('all'))
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
